103529a1edcbc2db2cb900a2d66a93697dcab2df
Elena <vega22.Elena@gmail.com>
2023-07-16 01:46:54 -0500

    created person class

--- homework.js
+++ homework.js
@@ -15,4 +15,23 @@ class  Hamster{
         }
           }
           const myHamster= new Hamster('Timmy','Gas');
-          
+          class Person{
+            constructor(name, age=0,height=0,weight=0,mood=0, hamsters=[],bankAccount=0)
+          {
+            this.name=name;
+            this.age=age;
+            this.height=height;
+            this.weight=weight;
+            this.mood=mood;
+            this.hamsters=hamsters;
+            this.bankAccount=bankAccount;}
+            getName(){return this.name;}
+getAge(){return this.age;}
+getWeight(){return this.weight;}
+greet(){console.log (this.name);}
+eat(){this.weight++; this.mood++;}
+exercise(){this.weight--;}
+ageUp(){this.age++; this.height++; this.weight++; this.mood--;  this.bankAccoun= this.bankAccoun+10 ;}
+buyHamster(myHamster) {this.hamsters.push(myHamster); this.mood= this.mood+10;  this.bankAccount= this.bankAccount-myHamster.getPrice();}
+        }
+       
